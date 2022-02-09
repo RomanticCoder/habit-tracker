7645e21f22f91782875ab7275c7caaf5b38eb0fb
lee <letmalikdothat@gmail.com>
2022-02-09 13:22:25 -0500

    navbar init

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+
+    render() {
+        console.log("hi")
+        return (
+            <>
+                <p>how are you?</p>
+                <h1>Habit Tracking</h1>
+            </>
+        );
+    }
+}
+
+export default Navbar;
